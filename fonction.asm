6addda4f78b4d0e4bbc8488beaf2af0ac89eaa5d
Manganese92 <103360340+Manganese92@users.noreply.github.com>
2023-02-21 19:33:37 +0100

    ajout de la première fonction et du début du code

--- fonction.asm
+++ fonction.asm
@@ -1,4 +1,17 @@
-global nom_fonction
-nom_fonction:
-; code de la fonction
+
+max_size 400
+
+global random_point
+
+random_point:
+
+    move dx,max_size
+    rdrand ax
+    cmp CF,1
+    jne rander_point ; est-ce possible ?
+    div  dx
+
+
+
+
 ret
--- main.asm
+++ main.asm
@@ -1,20 +1,45 @@
-extern printf
-extern scanf
-
-%define	BYTE	1
-
 global main
+
 
 section .data
 
 
-
 section .bss
-tabx:  	resb    3
-taby:  	resb    3
+count 1
+max_size 400 
+tabx:  	dw    3
+taby:  	dw    3
+
 
 section .text
 
 
 
 main:
+
+    nb_aleatoire_x:
+        call random_point
+        move tabx,dx
+        cmp count,3
+        je nb_aleatoire_y
+        add count
+        jmp nb_aleatoire_x
+    
+
+    nb_aleatoire_y:
+        call random_point
+        move tab_y,dx
+        cmp count,5
+        je min_max
+        add count
+        jmp nb_aleatoire_y
+
+
+
+
+; Pour fermer le programme proprement :
+mov    rax, 60         
+mov    rdi, 0
+syscall
+
+ret
